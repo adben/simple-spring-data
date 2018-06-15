4b8b97f8ce156e707838097bd7878e1076b325b0
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2018-06-15 12:53:04 +0200

    setup and entities

---
+++ src/main/java/nl/adben/persistence/entities/Rank.java
@@ -0,0 +1,5 @@
+package nl.adben.persistence.entities;
+
+public enum Rank {
+    ENSIGN, LIEUTENANT, COMMANDER, CAPTAIN, COMMODORE, ADMIRAL
+}
---
+++ src/test/java/nl/adben/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.adben.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersistenceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
