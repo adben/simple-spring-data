10362c4d0133ff6709b04f61515ca45c10a2fabb
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2018-06-15 13:27:25 +0200

    testing the persistence

--- src/main/java/nl/adben/persistence/dao/OfficerRepository.java
+++ src/main/java/nl/adben/persistence/dao/OfficerRepository.java
@@ -1,11 +1,14 @@
 package nl.adben.persistence.dao;
 
 import nl.adben.persistence.entities.Officer;
+import nl.adben.persistence.entities.Rank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OfficerRepository extends JpaRepository<Officer, Integer> {
     List<Officer> findByRank(@Param("rank") Rank rank);
     List<Officer> findByLast(@Param("last") String last);
