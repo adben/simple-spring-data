6168f90d31b4b1dc5a9918810ce13dddda5b8c90
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2018-06-15 12:58:11 +0200

    ordinal

--- src/main/java/nl/adben/persistence/entities/Rank.java
+++ src/main/java/nl/adben/persistence/entities/Rank.java
@@ -1,5 +1,8 @@
 package nl.adben.persistence.entities;
 
+import javax.persistence.Enumerated;
+
+@Enumerated(EnumType.STRING)
 public enum Rank {
     ENSIGN, LIEUTENANT, COMMANDER, CAPTAIN, COMMODORE, ADMIRAL
 }
